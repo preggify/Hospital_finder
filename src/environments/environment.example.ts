8d4118f99d2e48c94049ca00465655d22ea8e80e
temi724 <tayodaniel0@gmail.com>
2025-08-18 22:01:32 +0100

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/environments/environment.example.ts 	modified:   src/environments/environment.prod.ts

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,3 +1,5 @@
+// This is a template file. Copy to environment.ts and update values.
+// Environment variables will be loaded from the .env file
 export const environment = {
   production: false,
   apiUrl: 'YOUR_API_URL_HERE'
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
-//check this in
 export const environment = {
   production: true,
   apiUrl: 'https://www.preggifyapiservice.preggify.com/preggify/v1/'
